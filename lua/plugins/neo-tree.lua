28302e4c23086652ff4355c343c719c0721b16b7
Michal Stolarczyk <michal.stolarczyk@workday.com>
2025-05-06 22:37:03 -0600

    updates

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      close_if_last_window = true,
+      filesystem = {
+        hijack_netrw_behavior = "disabled",
+      },
+    },
+  },
+}
