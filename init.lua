65e29bca0ff4005e6151083a974884084ab42667
Michal Stolarczyk <kaaj@mm.st>
2026-01-07 22:43:55 -0700

    updating packages

--- init.lua
+++ init.lua
@@ -1,12 +1,25 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-if vim.g.neovide then
-  vim.opt.guifont = { "Berkeley Mono", ":h20" }
-end
+vim.opt.guifont = { "PragmataPro Mono", ":h28" }
+
+-- clipboard
+vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 
 -- pipe mostly used for external integrations with the editor (from within Godot, etc)
 local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
+
 if not vim.loop.fs_stat(pipepath) then
   vim.fn.serverstart(pipepath)
 end
