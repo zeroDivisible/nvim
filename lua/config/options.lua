8caeb4ae98cef69eaeb5d6d74265e61ac1466066
Michal Stolarczyk <kaaj@mm.st>
2026-02-24 09:10:17 -0700

    extra options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,5 +4,11 @@
 
 -- Don't show tabs/trailing whitespace
 vim.opt.list = false
-
 vim.opt.scrolloff = 15
+vim.opt.sidescrolloff = 10
+vim.opt.showmode = false
+
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = false
