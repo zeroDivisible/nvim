d3aca10dbac115e1f736b610d1b5a5db4bf48fee
Michal Stolarczyk <kaaj@mm.st>
2026-04-24 22:25:11 -0600

    Updates

---
+++ lua/plugins/refactoring.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      -- TODO: remove once LazyVim extras/editor/refactoring.lua adds this dependency
+      "lewis6991/async.nvim",
+    },
+  },
+}
