c943a36cde42f3348fe485d5b47e47f8f671af19
Michal Stolarczyk <michal.stolarczyk@workday.com>
2026-05-21 19:37:26 -0600

    updates

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -9,6 +9,34 @@ return {
     "oskarnurm/koda.nvim",
   },
   {
+    "vimcolorschemes/olive-crt.nvim",
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+  },
+  {
+    "datsfilipe/vesper.nvim",
+    opts = {
+      transparent = false,
+      italics = {
+        comments = false,
+        keywords = false,
+        functions = false,
+        strings = false,
+        variables = false,
+      },
+    },
+  },
+  {
+    "ayu-theme/ayu-vim",
+  },
+  {
     "rose-pine/neovim",
     name = "rose-pine",
     opts = {
