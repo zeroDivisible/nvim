5094bf4eb97f45d58877e32409f8d9d426d6fc02
Michal Stolarczyk <michal.stolarczyk@workday.com>
2026-08-21 12:42:01 -0600

    add sizing text

--- init.lua
+++ init.lua
@@ -3,6 +3,19 @@ require("config.lazy")
 
 vim.opt.guifont = { "Berkeley Mono", ":h20:b" }
 
+if vim.g.neovide then
+  local function resize_font(delta)
+    local size = tonumber(vim.o.guifont:match(":h(%d+)"))
+    if size then
+      vim.o.guifont = vim.o.guifont:gsub(":h%d+", ":h" .. (size + delta))
+    end
+  end
+
+  local mod = vim.fn.has("mac") == 1 and "D" or "C"
+  vim.keymap.set("n", "<" .. mod .. "-=>", function() resize_font(1) end)
+  vim.keymap.set("n", "<" .. mod .. "-->", function() resize_font(-1) end)
+end
+
 -- pipe mostly used for external integrations with the editor (from within Godot, etc)
 local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
 
